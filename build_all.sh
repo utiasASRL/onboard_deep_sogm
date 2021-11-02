7ce5de1a824dfee0fe3bc261bfc156f92fddec07
HuguesTHOMAS <huguesthomas218@gmail.com>
2021-11-02 17:03:28 -0400

    .

---
+++ build_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source "/opt/ros/melodic/setup.bash"
+source "/opt/ros/eloquent/setup.bash"
+
+#. install/setup.bash
+
+# pkill roscore
+# pkill rosmaster
+# pkill gzclient
+# pkill gzserver
+# pkill rviz
+
+# echo "Waiting"
+
+# until rostopic list; do sleep 0.5; done #wait until rosmaster has started
+
+echo ""
+echo "Go"
+echo ""
+
+colcon build --symlink-install
+
