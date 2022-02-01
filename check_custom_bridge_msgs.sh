cfadf6c55dbe587abe34cb5d307308a8204e7920
HuguesTHOMAS <huguesthomas218@gmail.com>
2022-02-01 16:21:51 -0500

    .

--- check_custom_bridge_msgs.sh
+++ check_custom_bridge_msgs.sh
@@ -5,11 +5,22 @@
 
 #cd ../bridge_ws
 
-echo "Bridged VoxGrid?"
-
-pwd
-
 source "install/setup.bash"
 
+echo ""
+echo "Bridged tf?"
 ros2 run ros1_bridge dynamic_bridge --print-pairs | grep "tf"
+
+echo ""
+echo "Bridged VoxGrid?"
 ros2 run ros1_bridge dynamic_bridge --print-pairs | grep "Vox"
+
+echo ""
+echo "Bridged PointCloud?"
+ros2 run ros1_bridge dynamic_bridge --print-pairs | grep "PointCloud"
+
+echo ""
+echo "Bridged ObstacleArrayMsg?"
+ros2 run ros1_bridge dynamic_bridge --print-pairs | grep "ObstacleArrayMsg"
+
+echo ""
