3f74f497ec13714526e178b6f1b0673d2d2884d7
HuguesTHOMAS <huguesthomas218@gmail.com>
2022-02-24 18:17:23 -0500

    .

--- scripts/teb_debug.sh
+++ scripts/teb_debug.sh
@@ -6,9 +6,11 @@ source "/opt/ros/$ROS_1_DISTRO/setup.bash"
 . "../../catkin_ws/install_isolated/setup.bash"
 
 echo "Running TEB debug"
-# roslaunch teb_local_planner test_optim_node.launch &
+roslaunch teb_local_planner test_optim_node.launch &
 
-# until rostopic list; do sleep 0.5; done #wait until rosmaster has started 
+until rostopic list; do sleep 0.5; done #wait until rosmaster has started 
+
+rosrun rqt_reconfigure rqt_reconfigure &
 
 rosrun teb_local_planner costmap_from_rosbag.py
  
