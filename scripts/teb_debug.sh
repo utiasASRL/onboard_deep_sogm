7918632a8d30fa160d003c5113a9bd3033788c32
HuguesTHOMAS <huguesthomas218@gmail.com>
2022-02-25 12:46:41 -0500

    .

--- scripts/teb_debug.sh
+++ scripts/teb_debug.sh
@@ -8,10 +8,13 @@ source "/opt/ros/$ROS_1_DISTRO/setup.bash"
 echo "Running TEB debug"
 roslaunch teb_local_planner test_optim_node.launch &
 
+echo "waiting for ros master"
 until rostopic list; do sleep 0.5; done #wait until rosmaster has started 
 
+echo "Start reconfigure"
 rosrun rqt_reconfigure rqt_reconfigure &
 
+echo "Start python rosbag publisher"
 rosrun teb_local_planner costmap_from_rosbag.py
  
 exit 1
