c088895154a9774af3b60d32cfdcdf053d91ac54
HuguesTHOMAS <huguesthomas218@gmail.com>
2022-05-30 10:54:01 -0400

    .

--- src/deep_sogm/deep_sogm/fast_collider.py
+++ src/deep_sogm/deep_sogm/fast_collider.py
@@ -171,7 +171,7 @@ class OnlineCollider(Node):
         # Init ROS #
         ############
 
-        self.static_range = 1.0
+        self.static_range = 1.1
         self.dynamic_range = 1.2
         self.dynamic_t_range = 1.0
         self.norm_p = 3
