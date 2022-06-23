700acffe81085bd43edabb2329aabeab517fdd06
HuguesTHOMAS <huguesthomas218@gmail.com>
2022-06-23 16:11:11 +0000

    .

--- src/deep_sogm/deep_sogm/fast_collider.py
+++ src/deep_sogm/deep_sogm/fast_collider.py
@@ -174,7 +174,7 @@ class OnlineCollider(Node):
         self.static_range = 1.1
         self.dynamic_range = 1.2  # TODO 1.2
         self.dynamic_t_range = 1.0  # TODO 1.0
-        self.norm_p = 1  # TODO 3
+        self.norm_p = 3  # TODO 3
         self.norm_invp = 1 / self.norm_p
 
         # self.maxima_layers = [18, 38]
